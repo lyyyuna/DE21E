0ed88e7d9a11051920d1c3e081ab10feb613bae3
lyyyuna <lyyyuna@gmail.com>
2017-05-20 09:30:35 +0800

    file and image

--- notes/flower/models.py
+++ notes/flower/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Article(models.Model):
     title = models.CharField(max_length=30, default='New')
+    createtime = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL) 
     content = models.TextField()
-    createtime = models.DateTimeField(auto_now=True)
+
     
     
 
---
+++ notes/trees/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class OFile(models.Model):
+    title = models.CharField(max_length=30)
+    ofile = models.FileField(upload_to="staticfile/%Y/%m/")
+    createtime = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return self.ofile.url
+
+
+class OImage(models.Model):
+    title = models.CharField(max_length=30)
+    oimage = models.FileField(upload_to="image/%Y/%m/")
+    createtime = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return self.oimage.url
