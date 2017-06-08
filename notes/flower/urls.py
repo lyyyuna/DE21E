7204debf210b60f6a1a7995d8052633fae35fd0b
lyyyuna <lyyyuna@gmail.com>
2017-06-08 22:43:26 +0800

    updates

--- notes/flower/urls.py
+++ notes/flower/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from . import customuploader_views
 
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     #url(r'^course/(?P<courseslug>[\w-]+)/$', views.get_course, name='course_detail'),
     url(r'^tutorial/(?P<articleslug>[\w-]+)/$', views.get_article, name='article_detail'),
     url(r'^$', views.index, name='index'),
+    url(r'^api/uploader/$', customuploader_views.markdown_uploader, name='markdown_uploader_page'),
 ]
